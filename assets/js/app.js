b58b885fd8e8dd02e233e4e85269d29600815a1a
sunt1ago <159410230+sunt1ago@users.noreply.github.com>
2024-02-19 19:45:01 +0500

    clearing js

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,26 +22,6 @@ $(function() {
         }
     }
 
-
-
-    /* Smooth scroll */
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('scroll'),
-            blockOffset = $(blockId).offset().top;
-
-        $("#nav a").removeClass("active");
-        $this.addClass("active");
-
-        $("html, body").animate({
-            scrollTop:  blockOffset
-        }, 500);
-    });
-
-
-
     /* Menu nav toggle */
     $("#nav_toggle").on("click", function(event) {
         event.preventDefault();
@@ -49,26 +29,4 @@ $(function() {
         $(this).toggleClass("active");
         $("#nav").toggleClass("active");
     });
-
-
-
-    /* Collapse */
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('collapse');
-
-        $this.toggleClass("active");
-    });
-
-
-    /* Slider */
-    $("[data-slider]").slick({
-        infinite: true,
-        fade: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-
 });
